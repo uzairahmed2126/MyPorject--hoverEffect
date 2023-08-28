9d191ee7e8b71aea85b0a98549e1263d12de59c9
uzairahmed2126 <uzairahmed21261@gmail.com>
2023-08-28 18:41:28 +0530

    hover effect

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// Targets
+let bdy = document.querySelector('body')
+
+// createElements
+const btn = document.createElement('button')
+const div = document.createElement('div')
+let container = document.getElementById('container')
+
+// appendChild btn
+bdy.appendChild(btn)
+btn.innerText = 'Hover and see'
+btn.setAttribute('id', 'hoverEffect')
+bdy.appendChild(div)
+container.appendChild(btn)
+container.appendChild(div)
+// trial
+
+// Create Div
+const Idbtn = document.getElementById('hoverEffect')
+Idbtn.addEventListener('mouseenter', () => {
+    div.style.height = '300px'
+    div.style.width = '300px'
+    div.style.display = 'flex'
+    div.style.alignItems = 'center'
+    div.style.justifyContent = 'center'
+    div.style.fontSize = '90px'
+    div.style.border = 'none'
+    div.style.borderRadius = '30px'
+    div.style.visibility = 'visible'
+    div.style.transition = '1s ease-in-out'
+    div.innerHTML = '<iframe id="frame" width="560" height="315" src="https://www.youtube.com/embed/qjRQAjbtb8U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen"></iframe>'
+})
+
+
+div.addEventListener('mouseenter', () => {
+    div.style.visibility = 'visible'
+})
+
+div.addEventListener('mouseleave', () => {
+    div.style.visibility = 'hidden'
+})
+
+// Remove Div
+Idbtn.addEventListener('mouseleave', () => {
+    // bdy.removeChild(div)
+    div.style.visibility = 'hidden'
+})
